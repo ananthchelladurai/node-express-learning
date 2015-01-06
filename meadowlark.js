a427b8dca060d37e30275c9a75a54f1d5267d9cf
Ananth Chelladurai <achelladurai@ebay.com>
2015-01-06 15:27:59 -0800

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,14 +1,6 @@
 var express = require('express');
-
 var app = express();
-
-var fortunes = [
-        "Conquer your fears or they will conquer you.",
-        "Rivers need springs.",
-        "Do not fear what you don't know.",
-        "You will have a pleasant surprise.",
-        "Whenever possible, keep it simple.",
-];
+var fortune = require('./lib/fortune.js');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -24,8 +16,7 @@ app.get('/', function(req, res) {
      res.render('home');
 });
 app.get('/about', function(req, res) {
-      var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-      res.render('about', { fortune: randomFortune });
+      res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
