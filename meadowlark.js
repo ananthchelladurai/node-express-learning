d8969b7a158a94856996d58236e807a3e9490fb9
Ananth Chelladurai <achelladurai@ebay.com>
2015-01-08 15:52:34 -0800

    Tried grunt and jshint for the app.

--- meadowlark.js
+++ meadowlark.js
@@ -4,19 +4,38 @@ var fortune = require('./lib/fortune.js');
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 // set up handlebars view engine
-var handlebars = require('express-handlebars')
-        .create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
      res.render('home');
 });
+
 app.get('/about', function(req, res) {
-      res.render('about', { fortune: fortune.getFortune() });
+  res.render('about', { 
+    fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
+  });
+});
+
+app.get('/tours/hood-river', function(req, res) {
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function(req, res) {
+  res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function(req, res) {
+  res.render('tours/request-group-rate');
 });
 
 // 404 catch-all handler (middleware)
@@ -32,7 +51,6 @@ app.use(function(err, req, res, next){
         res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.' );
+app.listen(app.get('port'), function() {
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
